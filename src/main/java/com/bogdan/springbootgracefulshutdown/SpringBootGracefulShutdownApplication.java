bdf049c8e83a92e578e7ca023d1799e7ae2c97f7
zoomout <bzastavnyi@gmail.com>
2019-10-12 21:28:43 +0200

    initcommit

---
+++ src/main/java/com/bogdan/springbootgracefulshutdown/SpringBootGracefulShutdownApplication.java
@@ -0,0 +1,13 @@
+package com.bogdan.springbootgracefulshutdown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGracefulShutdownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGracefulShutdownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bogdan/springbootgracefulshutdown/SpringBootGracefulShutdownApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bogdan.springbootgracefulshutdown;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootGracefulShutdownApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
